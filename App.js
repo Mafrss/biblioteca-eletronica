198d29fd1b2c4b62bbcc3c34c81df1d0ef98c9b7
Mafrss <88444242+Mafrss@users.noreply.github.com>
2022-04-22 00:58:25 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+    //Adicione a instrução de retorno para BottomTabNavigator - Desafio 4
+  }
+}
